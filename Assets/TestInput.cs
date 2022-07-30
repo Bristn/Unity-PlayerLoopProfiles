c22b2de970b167e00ea0431d29d683aec758ad4e
Neher <roman.neher@hs-weingarten.de>
2022-07-30 20:35:19 +0200

    Move interaction code to one method

--- Assets/TestInput.cs
+++ Assets/TestInput.cs
@@ -19,7 +19,6 @@ public class TestInput : MonoBehaviour
         MIDDLE_CLICK,
         CLICK,
         SCROLL_WHEEL,
-        MOVE,
         SUBMIT,
         CANCEL,
         TRACKED_DEVICE_POSITION,
@@ -34,7 +33,6 @@ public class TestInput : MonoBehaviour
         "MiddleClick",
         "Click", 
         "ScrollWheel", 
-        "Move", 
         "Submit", 
         "Cancel", 
         "TrackedDevicePosition",
@@ -52,7 +50,6 @@ public class TestInput : MonoBehaviour
         actions.Add(actionNames[(int)ActionType.MIDDLE_CLICK], MiddleClick);
         actions.Add(actionNames[(int)ActionType.CLICK], Click);
         actions.Add(actionNames[(int)ActionType.SCROLL_WHEEL], ScrollWheel);
-        actions.Add(actionNames[(int)ActionType.MOVE], Move);
         actions.Add(actionNames[(int)ActionType.SUBMIT], Submit);
         actions.Add(actionNames[(int)ActionType.CANCEL], Cancel);
         actions.Add(actionNames[(int)ActionType.TRACKED_DEVICE_POSITION], TrackedDevicePosition);
@@ -80,70 +77,29 @@ public class TestInput : MonoBehaviour
         }
     }
 
+    public void Navigate(CallbackContext pContext) => Interaction(pContext, ActionType.NAVIGATE);
 
-    public void Navigate(CallbackContext pContext)
-    {
-        Debug.Log("Navigate");
-        LowPowerManager.Instance.playerLoopManager.SetActiveProfile(Profile.NORMAL);
-    }
+    public void Point(CallbackContext pContext) => Interaction(pContext, ActionType.POINT);
 
-    public void Point(CallbackContext pContext)
-    {
-        Debug.Log("Point");
-        LowPowerManager.Instance.playerLoopManager.SetActiveProfile(Profile.NORMAL);
-    }
+    public void Click(CallbackContext pContext) => Interaction(pContext, ActionType.CLICK);
 
-    public void Click(CallbackContext pContext)
-    {
-        Debug.Log("Click");
-        LowPowerManager.Instance.playerLoopManager.SetActiveProfile(Profile.NORMAL);
-    }
+    public void MiddleClick(CallbackContext pContext) => Interaction(pContext, ActionType.MIDDLE_CLICK);
 
-    public void MiddleClick(CallbackContext pContext)
-    {
-        Debug.Log("MiddleClick");
-        LowPowerManager.Instance.playerLoopManager.SetActiveProfile(Profile.NORMAL);
-    }
+    public void RightClick(CallbackContext pContext) => Interaction(pContext, ActionType.RIGHT_CLICK);
 
-    public void RightClick(CallbackContext pContext)
-    {
-        Debug.Log("RightClick");
-        LowPowerManager.Instance.playerLoopManager.SetActiveProfile(Profile.NORMAL);
-    }
+    public void ScrollWheel(CallbackContext pContext) => Interaction(pContext, ActionType.SCROLL_WHEEL);
 
-    public void ScrollWheel(CallbackContext pContext)
-    {
-        Debug.Log("ScrollWheel");
-        LowPowerManager.Instance.playerLoopManager.SetActiveProfile(Profile.NORMAL);
-    }
+    public void Submit(CallbackContext pContext) => Interaction(pContext, ActionType.SUBMIT);
 
-    public void Move(CallbackContext pContext)
-    {
-        Debug.Log("Move");
-        LowPowerManager.Instance.playerLoopManager.SetActiveProfile(Profile.NORMAL);
-    }
+    public void Cancel(CallbackContext pContext) => Interaction(pContext, ActionType.CANCEL);
 
-    public void Submit(CallbackContext pContext)
-    {
-        Debug.Log("Submit");
-        LowPowerManager.Instance.playerLoopManager.SetActiveProfile(Profile.NORMAL);
-    }
+    public void TrackedDevicePosition(CallbackContext pContext) => Interaction(pContext, ActionType.TRACKED_DEVICE_POSITION);
 
-    public void Cancel(CallbackContext pContext)
-    {
-        Debug.Log("Cancel");
-        LowPowerManager.Instance.playerLoopManager.SetActiveProfile(Profile.NORMAL);
-    }
+    public void TrackedDeviceOrientation(CallbackContext pContext) => Interaction(pContext, ActionType.TRACKED_DEVICE_ORIENTATION);
 
-    public void TrackedDevicePosition(CallbackContext pContext)
+    private void Interaction(CallbackContext pContext, ActionType pType)
     {
-        Debug.Log("TrackedDevicePosition");
-        LowPowerManager.Instance.playerLoopManager.SetActiveProfile(Profile.NORMAL);
-    }
-
-    public void TrackedDeviceOrientation(CallbackContext pContext)
-    {
-        Debug.Log("TrackedDeviceOrientation");
+        Debug.Log(pType);
         LowPowerManager.Instance.playerLoopManager.SetActiveProfile(Profile.NORMAL);
     }
 }
